0d1a6c10ccb298eed155e1f98a6a731caea83fd9
prncssll <princessleila.ditchon@cvsu.edu.ph>
2022-11-30 20:14:38 +0800

    First commit

---
+++ bmi/BMI.java
@@ -0,0 +1,16 @@
+package bmi;
+
+public class BMI {
+   
+    private double height, weight;
+
+    public void setHW (double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double  setBMI ()
+    
+        
+    
+}
